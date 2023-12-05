d64aa37c42007b689339e343b7a395a64c766613
Shahmahdi <shahmahdi11@gmail.com>
2023-12-05 10:27:04 +0600

    minor change

--- src/actions/zero.js
+++ src/actions/zero.js
@@ -4,7 +4,7 @@ export const getTokenFromXero = async () => {
   try {
     const redirectUrl = "https://xero-oauth2-connection.vercel.app";
     const clientId = "86AFC373A4E642A48A6501314FB0255C";
-    const url = `https://login.xero.com/identity/connect/authorize?response_type=code&client_id=${clientId}&redirect_uri=${redirectUrl}&scope=profile&state=123`;
+    const url = `https://login.xero.com/identity/connect/authorize?response_type=code&client_id=${clientId}&redirect_uri=${redirectUrl}&scope=openid profile email&state=123`;
     console.log(`url: `, url);
     const res = await axios.get(url);
     console.log("======", res);
