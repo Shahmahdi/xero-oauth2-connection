52f14a88a312ddb6c50fd5708b2bd39935e0368f
Shahmahdi <shahmahdi11@gmail.com>
2023-12-05 10:18:37 +0600

    minor change

--- src/App.jsx
+++ src/App.jsx
@@ -33,7 +33,7 @@ function App() {
         Click on the Vite and React logos to learn more
       </p>
       <p className="read-the-docs">
-        Hello world
+        Hello mahdi
       </p>
     </>
   )
